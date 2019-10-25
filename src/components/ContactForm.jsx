8d73d050b47a53621f58565dbb7400d6c6e9c5d5
Jay Billings <contact@scribbleowl.com>
2019-10-24 20:52:53 -0700

    Contact form sends email via new service

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import app from '../services/socketio';
 
 import '../styles/contact-form.css';
 
@@ -6,28 +7,70 @@ export default class ContactForm extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {submitStatus: null}; // null = unsent, 1 = sent, 0 = error
+
     this.nameRef = React.createRef();
     this.emailRef = React.createRef();
     this.msgRef = React.createRef();
+
+    this.mailerService = app.service('send-mail');
+
+    this.sendMessage = this.sendMessage.bind(this);
+    this.clearForm = this.clearForm.bind(this);
+    this.renderSendStatus = this.renderSendStatus.bind(this);
+  }
+
+  sendMessage(e) {
+    e.preventDefault();
+
+    const email = {
+      from: this.emailRef.current.value,
+      body: this.msgRef.current.value
+    };
+
+    this.mailerService.create(email).then(result => {
+      console.info(result);
+      this.clearForm();
+      this.setState({submitStatus: 1});
+    }).catch(err => {
+      console.error(err);
+      this.setState({submitStatus: 0});
+    });
+  }
+
+  clearForm() {
+    this.nameRef.current.value = '';
+    this.emailRef.current.value = '';
+    this.msgRef.current.value = '';
+  }
+
+  renderSendStatus() {
+    if (this.state.submitStatus) return 'Message sent. Thank you for your message.';
+    if (this.state.submitStatus !== null) return 'Message could not be sent. Please try again.';
+
+    return '';
   }
 
   render() {
     return (
-      <form className={'contactForm'}>
-        <label>
-          <span>Name</span>
-          <input type={'text'} name={'contact_name'} ref={this.nameRef} />
-        </label>
-        <label>
-          <span>Email Address</span>
-          <input type={'email'} name={'email'} ref={this.emailRef} />
-        </label>
-        <label>
-          <span>Message</span>
-          <textarea ref={this.msgRef} />
-        </label>
-        <button type={'submit'}>Send Message</button>
-      </form>
+      [
+        <div className={'formMsg'}>{this.renderSendStatus()}</div>,
+        <form className={'contactForm'} onSubmit={this.sendMessage}>
+          <label>
+            <span>Name</span>
+            <input type={'text'} name={'contact_name'} ref={this.nameRef}/>
+          </label>
+          <label>
+            <span>Email Address</span>
+            <input type={'email'} name={'email'} ref={this.emailRef}/>
+          </label>
+          <label>
+            <span>Message</span>
+            <textarea ref={this.msgRef}/>
+          </label>
+          <button type={'submit'}>Send Message</button>
+        </form>
+      ]
     )
   }
 }
