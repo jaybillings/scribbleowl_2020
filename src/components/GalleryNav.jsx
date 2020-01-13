99d0620e98cd88073c83be578e5705afa8d8f55e
Jay Billings <contact@scribbleowl.com>
2020-01-13 00:11:51 -0800

    Visual tweaks to gallery nav

--- src/components/GalleryNav.jsx
+++ src/components/GalleryNav.jsx
@@ -15,21 +15,21 @@ export default class GalleryNav extends Component {
 
     let navItems = [];
 
-    if (projIndex > 0) navItems.push(<li key={'prevproj'} className={'left'}><Link
+    if (projIndex > 0) navItems.push(<li key={'prevproj'}><Link
       to={`/gallery/${this.props.projList[projIndex - 1]}/`} className={'left'}>&lt;&lt; Prev Project</Link></li>);
     else navItems.push(<li key={'prevproj'} className={'left inactive'}>&lt;&lt; Prev Project</li>);
 
-    if (this.props.imgIndex > 0) navItems.push(<li key={'previmg'} className={'left'}><Link
+    if (this.props.imgIndex > 0) navItems.push(<li key={'previmg'}><Link
       to={`/gallery/${this.props.projID}/${this.props.imgIndex - 1}`} className={'left'}>&lt; Prev image</Link></li>);
     else navItems.push(<li key={'previmg'} className={'left inactive'}>&lt; Prev image</li>);
 
     navItems.push(<li key={'home'} className={'center'}><Link to={'/'}>Home</Link></li>);
 
-    if (this.props.imgIndex < this.props.images.length) navItems.push(<li key={'nextimg'} className={'right'}><Link
+    if (this.props.imgIndex < this.props.images.length - 1) navItems.push(<li key={'nextimg'}><Link
       to={`/gallery/${this.props.projID}/${this.props.imgIndex + 1}`} className={'right'}>Next image &gt;</Link></li>);
     else navItems.push(<li key={'nextimg'} className={'right inactive'}>Next image &gt;</li>);
 
-    if (projIndex < this.props.projList.length) navItems.push(<li key={'nextproj'} className={'right'}><Link
+    if (projIndex < this.props.projList.length - 1) navItems.push(<li key={'nextproj'}><Link
       to={`/gallery/${this.props.projList[projIndex + 1]}`} className={'right'}>Next Project &gt;&gt;</Link></li>);
     else navItems.push(<li key={'nextproj'} className={'right inactive'}>Next Project &gt;&gt;</li>);
 
