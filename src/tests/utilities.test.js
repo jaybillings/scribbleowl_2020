167a1642f2bff05a89422268b80c428752710ac5
Jay Billings <contact@scribbleowl.com>
2020-01-27 23:15:32 -0800

    Adds case for utilities.js testing

--- src/tests/utilities.test.js
+++ src/tests/utilities.test.js
@@ -3,7 +3,12 @@ import {renderCopy} from "../js/utilities";
 
 it("properly transforms paragraphs", () => {
   const copy = ["some test text", "some more test text"];
-  let result = renderCopy(copy);
+  let result;
 
+  // Without provided key
+  result = renderCopy(copy);
   expect(result).toEqual([<p key={'copy_0'}>some test text</p>,<p key={'copy_1'}>some more test text</p>]);
+  // With provided key
+  result = renderCopy(copy, 'test');
+  expect(result).toEqual([<p key={'copy_test_0'}>some test text</p>,<p key={'copy_test_1'}>some more test text</p>]);
 });
