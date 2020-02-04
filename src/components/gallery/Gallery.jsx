06c66b75e7bce6edb4397823ba72e72d2995eb7f
Jay Billings <contact@scribbleowl.com>
2020-02-03 21:09:56 -0800

    Minor updates to gallery component
    * Adds live link, where applicable

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -5,10 +5,12 @@ import "../../styles/gallery.css";
 
 export default function Gallery(props) {
   const currentImage = props.images[props.imgIndex];
+  const projLink = props.project.uri ? <p className={'live-link'}>Visit live version at <a href={props.project.uri} target={'_blank'}>{props.project.uri}</a>.</p> : '';
 
   return (
     <div className={'gallery'}>
       <p className={'tech'}><strong>{props.project.tech.join(', ')}</strong></p>
+      {projLink}
       {renderCopy(props.project.copy)}
       <div className={'galleryInner'}>
         <figure><img alt={''} src={currentImage.name}/></figure>
