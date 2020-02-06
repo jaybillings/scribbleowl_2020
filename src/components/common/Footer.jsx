4cfb655d5c424588f1f6ef775ba2400a8758a3c3
Jay Billings <contact@scribbleowl.com>
2020-02-06 01:44:03 -0800

    Adds media breakpoints to main page

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer(props) {
     <footer>
       <p>By Jay Billings, 2019-2020.</p>
       <p>View source and copyright <a href={'https://github.com/jaybillings/scribbleowl_2020'}
-                                      target={'_blank'}>on GitHub.</a></p>
+                                      target={'_blank'}>on GitHub</a>.</p>
     </footer>
   )
 }
