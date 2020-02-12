d26b3a682d7992060d2c78efb7b1538a80203714
Jay Billings <contact@scribbleowl.com>
2020-02-11 20:18:09 -0800

    Fix for issue where error pages don't provide props for header
    * Uses default if no props provided

--- src/layouts/ErrorPage.jsx
+++ src/layouts/ErrorPage.jsx
@@ -5,8 +5,11 @@ import Header from "../components/Header";
 import Footer from "../components/common/Footer";
 
 export default function ErrorPage(props) {
+  const title = props.title ?? 'Jay Billings';
+  const navSections = props.navSections ?? [];
+
   return [
-    <Header key={'header'} />,
+    <Header key={'header'} title={title} navSections={navSections} />,
     <div key='content' className={'section'}>
       <h2>Oops, something went wrong.</h2>
       <p>Sorry about that. <Link to={'/'}>Return home.</Link></p>
--- src/layouts/NotFoundPage.jsx
+++ src/layouts/NotFoundPage.jsx
@@ -5,8 +5,11 @@ import Header from "../components/Header";
 import Footer from "../components/common/Footer";
 
 export default function NotFoundPage(props) {
+  const title = props.title ?? 'Jay Billings';
+  const navSections = props.navSections ?? [];
+
   return [
-    <Header key={'header'} />,
+    <Header key={'header'} title={title} navSections={navSections} />,
     <div key={'content'} className={'section'}><h2>Sorry, that page could not be found. <Link to={'/'}>Return home.</Link></h2></div>,
     <Footer key={'footer'} />
   ]
