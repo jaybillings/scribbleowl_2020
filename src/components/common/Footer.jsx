d62b5c80380bb0d2525dfed9007698b4266779d1
Jay Billings <contact@scribbleowl.com>
2020-02-18 16:27:51 -0800

    MInor footer update
    * Link extends across entire sentence

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -6,8 +6,8 @@ export default function Footer(props) {
   return (
     <footer>
       <p>By Jay Billings, 2019-2020.</p>
-      <p>View source and copyright <a href={'https://github.com/jaybillings/scribbleowl_2020'}
-                                      target={'_blank'}>on GitHub</a>.</p>
+      <p><a href={'https://github.com/jaybillings/scribbleowl_2020'} target={'_blank'}>View source and copyright on
+        GitHub</a>.</p>
     </footer>
   )
 }
