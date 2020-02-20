0661c75e20c007ff025aaa8e3f55e3230ac77fe9
Jay Billings <contact@scribbleowl.com>
2020-02-19 17:38:40 -0800

    Adds copy to about and contact sections and tweaks same components

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
-import {renderCopy} from "../../js/utilities";
+import React, { Component } from 'react';
 
 import ScrollTop from "../common/ScrollTop";
+import ReactMarkdown from "react-markdown";
+
+import "../../styles/scss/about.scss";
 
 export default class About extends Component {
   constructor(props) {
@@ -15,20 +17,20 @@ export default class About extends Component {
 
     this.props.skillsSection.forEach((section, index) => {
       html.push(<h3 key={`header_${index}`}>{section.header}</h3>);
-      html.push(renderCopy(section.copy, 'aboutskills'));
+      html.push(<ReactMarkdown key={`copy_${index}`} source={section.copy} />);
     });
 
     return html.map(elem => elem);
   }
 
   render() {
-    return ([
-      <div id={'about'} key={'about'} className={'section'}>
+    return (
+      <div id={'about'} key={'about'} className={'about section'}>
         <h2>{this.props.title}</h2>
-        {this.props.copy.map((line, index) => <p key={`aboutmain_${index}`}>{line}</p>)}
+        <ReactMarkdown source={this.props.copy} />
         {this.renderSkillsSection()}
-        <ScrollTop/>
+        <ScrollTop />
       </div>
-    ])
+    )
   }
 }
--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import {renderCopy} from "../../js/utilities"
+import {Link} from "react-router-dom"
 
 import ContactForm from "../ContactForm"
 import ScrollTop from "../common/ScrollTop"
-import {Link} from "react-router-dom";
+import ReactMarkdown from "react-markdown"
 
 export default function Contact(props) {
+  const cta = props.forHire ? props.forHireCTA : props.noHireCTA;
+
   return (
     <div id={'contact-section'} className={'contact section'}>
       <h2><Link id={'contact'} to={'#contact'} className={'sr-only show-on-focus'}>#</Link> {props.title}</h2>
-      {renderCopy(props.copy)}
-      <p>All fields are required.</p>
+      <ReactMarkdown source={cta} />
+      <p className={'important'}>* All fields are required.</p>
       <ContactForm/>
       <ScrollTop/>
     </div>
